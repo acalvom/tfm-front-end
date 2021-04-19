0260316930775083b6c2135d40c9664bdbd3d532
acalvom <andreac.3108@gmail.com>
2021-04-19 11:39:56 +0200

    set app theme & improve navbar #5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularMaterialModule} from './shared/angular-material.module';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
-import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -20,8 +19,7 @@ import {ReactiveFormsModule} from '@angular/forms';
     AngularMaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    BrowserModule,
-    ReactiveFormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
