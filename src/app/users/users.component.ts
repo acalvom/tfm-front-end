6babda6d2e0d2e03502880f530fd5af851802e46
acalvom <andreac.3108@gmail.com>
2021-04-27 18:30:00 +0200

    create table to list users #3

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../shared/models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  users: User[] = [];
+  columns: string[] = ['name', 'email', 'dni', 'role'];
+  dataSource = this.getStudents();
+
+  getStudents() {
+    this.users = [
+      {name: 'a', surname: 'aa', dni: '1234A', gender: 'man', email: 'a@a', password: 'a', penalties: 0, role: 'teacher'},
+      {name: 'b', surname: 'bb', dni: '1234B', gender: 'woman', email: 'b@b', password: 'b', penalties: 2, role: 'student'}
+    ];
+    return this.users;
+  }
 
   ngOnInit(): void {
   }
