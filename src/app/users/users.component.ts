956337757b6cecd189c7b3832ad9ea1bb2ee08d6
acalvom <andreac.3108@gmail.com>
2021-04-28 18:35:46 +0200

    refactor filter & implement sort (in progress) #7

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {UsersService} from '../shared/services/users.service';
 import {MatTableDataSource} from '@angular/material/table';
 import {User} from '../shared/models/user.model';
 import {MatSort} from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-users',
@@ -11,13 +12,13 @@ import {MatSort} from '@angular/material/sort';
 })
 export class UsersComponent implements OnInit {
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   columns: string[] = ['name', 'surname', 'gender', 'email', 'dni', 'penalties'];
   users: User[] = [];
-
   students = new MatTableDataSource<User>();
   teachers = new MatTableDataSource<User>();
-  dataSource = new MatTableDataSource<User>();
-
 
   constructor(private userService: UsersService) {
   }
@@ -27,25 +28,15 @@ export class UsersComponent implements OnInit {
     this.getTeachers();
   }
 
-  applyFilterStudent(event: Event) {
-    const filtro = (event.target as HTMLInputElement).value;
-    this.students.filter = filtro.trim().toLowerCase();
-  }
-
-  applyFilterTeacher(event: Event) {
-    const filtro = (event.target as HTMLInputElement).value;
-    this.teachers.filter = filtro.trim().toLowerCase();
+  applyFilter(event: Event, dataSource: MatTableDataSource<User>) {
+    const filter = (event.target as HTMLInputElement).value;
+    dataSource.filter = filter.trim().toLowerCase();
   }
 
   getStudents() {
     this.userService.getStudents().subscribe(
       (response: any) => {
-        this.users = [];
-        for (let key in response.body) {
-          let user = new User();
-          user.copyProperties(response.body[key]);
-          this.users.push(user);
-        }
+        this.generateUserFromArray(response.body);
         this.students.data = this.users as User[];
       });
   }
@@ -53,59 +44,18 @@ export class UsersComponent implements OnInit {
   getTeachers() {
     this.userService.getTeachers().subscribe(
       (response: any) => {
-        this.users = [];
-        for (let key in response.body) {
-          let user = new User();
-          user.copyProperties(response.body[key]);
-          this.users.push(user);
-        }
+        this.generateUserFromArray(response.body);
         this.teachers.data = this.users as User[];
       });
   }
+
+  generateUserFromArray(anyArray: any){
+    this.users = [];
+    for (let key in anyArray) {
+      let user = new User();
+      user.copyProperties(anyArray[key]);
+      this.users.push(user);
+    }
+  }
 }
 
-//   constructor(private userService: UsersService) {
-//   }
-//   students: any[] = [];
-//   teachers: any[];
-//   columns: string[] = ['name','surname','gender','email', 'dni', 'penalties'];
-//   getStudentsStatusCode;
-//   public dataSource = new MatTableDataSource<User>();
-//   sort: MatSort;
-//
-//   public doFilter = (value: string) => {
-//     this.dataSource.filter = value.trim().toLocaleLowerCase();
-//   }
-//
-//   applyFilter(filterValue: String) {
-//     console.log(filterValue)
-//     console.log(this.dataSource)
-//     this.dataSource.filter = filterValue.trim().toLowerCase();
-//   }
-//
-//   getStudents() {
-//     this.userService.getStudents().subscribe(
-//       (value: any) => {
-//         this.students = value.body;
-//         this.dataSource.data = value as User[];
-//       });
-//   }
-//
-//   getTeachers() {
-//     this.userService.getTeachers().subscribe(
-//       (value:any) => {
-//         this.teachers = value.body;
-//       });
-//   }
-//
-//
-//   ngOnInit(): void {
-//     this.getStudents();
-//     this.getTeachers();
-//
-//   }
-//   ngAfterViewInit(): void {
-//     this.dataSource.sort = this.sort;
-//   }
-//
-// }
