34807ca99fc404e9fab5ba987008d500de57ae9d
acalvom <andreac.3108@gmail.com>
2021-05-08 00:40:19 +0200

    fix "Add Phone" #12

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -49,12 +49,15 @@ export class ProfileComponent implements OnInit {
     this.dialog.open(AddPhoneDialogComponent)
       .afterClosed().subscribe(
       (phone: string) => {
-        this.userService.addPhone(this.user.email, phone).subscribe(() => {
-          this.getUserInformation();
-          this.snackBar.open('Phone successfully added', 'OK', {duration: 5000});
-        }, (error) => {
-          this.snackBar.open('Phone cannot be added: Error ' + error.status, 'OK', {duration: 5000});
-        });
+        if (phone) {
+          this.userService.addPhone(this.user.email, phone).subscribe(() => {
+            this.getUserInformation();
+            this.snackBar.open('Phone successfully added', 'OK', {duration: 5000});
+          }, (error) => {
+            this.snackBar.open('Phone cannot be added: Error ' + error.status, 'OK', {duration: 5000});
+          });
+        }
+
       });
   }
 }
