bcea08e2c9bb97481110ec48f850cae3b7602100
acalvom <andreac.3108@gmail.com>
2021-05-16 17:23:23 +0200

    implement service to create a new class #21

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -4,6 +4,7 @@ import {MatDialog} from '@angular/material/dialog';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {CreateClassDialogComponent} from '../shared/components/create-class-dialog/create-class-dialog.component';
 import {Class} from '../shared/models/class.model';
+import {ClassesService} from '../shared/services/classes.service';
 
 @Component({
   selector: 'app-classes',
@@ -13,6 +14,7 @@ import {Class} from '../shared/models/class.model';
 export class ClassesComponent implements OnInit {
 
   constructor(public authService: AuthService,
+              private classesService: ClassesService,
               private dialog: MatDialog,
               private snackBar: MatSnackBar) {
   }
@@ -24,8 +26,12 @@ export class ClassesComponent implements OnInit {
     this.dialog.open(CreateClassDialogComponent).afterClosed().subscribe(
       (newClass: Class) => {
         if (newClass) {
-          console.log('newClass');
           console.log(newClass);
+          this.classesService.createClass(newClass).subscribe(() => {
+            this.snackBar.open('Class successfully created', 'OK', {duration: 3000});
+          }, (error) => {
+            this.snackBar.open('Class cannot be created: Error ' + error.status, 'OK', {duration: 3000});
+          });
         }
       });
   }
