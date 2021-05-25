51eaf1c70545219f58f2d3f05ad69153df99ced6
acalvom <andreac.3108@gmail.com>
2021-05-25 18:49:55 +0200

    configure the table to get the users information to display #35

--- src/app/reserves-users/reserves-users.component.ts
+++ src/app/reserves-users/reserves-users.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {Reserve} from '../shared/models/reserve.model';
 import {ActivatedRoute} from '@angular/router';
+import {ReservesService} from '../shared/services/reserves.service';
+import {UsersService} from '../shared/services/users.service';
+import {StudentBasic} from '../shared/models/student-basic.model';
 
 @Component({
   selector: 'app-reserves-users',
@@ -10,15 +13,46 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ReservesUsersComponent implements OnInit {
 
-  columns: string[] = ['name', 'surname', 'dni'];
-  dataSource = new MatTableDataSource<Reserve>();
+  columns: string[] = ['name', 'surname', 'email'];
+  students: StudentBasic[] = [];
+  dataSource = new MatTableDataSource<StudentBasic>();
   code: string;
+  reserves: Reserve[] = [];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private reservesService: ReservesService,
+              private userService: UsersService) {
   }
 
   ngOnInit(): void {
     this.code = this.route.snapshot.paramMap.get('code');
+    this.getReservesByCodeClass(this.code);
+  }
+
+  getReservesByCodeClass(code: string) {
+    this.reservesService.getReservesByCodeClass(code).subscribe(
+      (response: any) => {
+        this.reserves = response.body;
+        this.generateStudentBasicFromArray(this.reserves);
+      });
+  }
+
+  getStudentInfo(email: string) {
+    this.userService.getUserByEmail(email).subscribe(
+      (response: any) => {
+        let student = new StudentBasic();
+        student.copyProperties(response.body[0]);
+        this.students.push(student);
+        this.dataSource.data = this.students;
+      });
+  }
+
+  generateStudentBasicFromArray(anyArray: any) {
+    this.students = [];
+    for (let key in anyArray) {
+      let studentEmail = anyArray[key].email_user;
+      this.getStudentInfo(studentEmail);
+    }
   }
 
 }
